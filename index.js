10a51e0a39bc3ebc49c436b8947fef133266df9d
nicksolony <nicksolony@gmail.com>
2020-07-20 21:56:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array,fn) {
+  let newArray=[];
+  array.forEach((item) => {
+    newArray.push(fn(item));
+  });
+return newArray;
+};
+
+
+function reduce(array,fn,start) {
+  let s = (!!start)? start:array[0];
+  let i = (!!start)? 0:1;
+  for ( ;i < array.length; i++) {
+    s = fn(array[i],s)
+  };
+  return s;
+};
